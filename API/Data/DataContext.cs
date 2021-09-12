7e1ee0fb25c0b73f4f06b1cb18c0dee6debe8652
nuklearna <n.swenda@gmail.com>
2021-09-12 17:20:42 +0200

    Update DB, querry for insert into table

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,13 +1,10 @@
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
-
 namespace API.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions options) : base(options)
-        {
-        }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
 
         public DbSet<AppUser> User { get; set; }
     }
--- API/Startup.cs
+++ API/Startup.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,6 +12,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.Data.Sqlite;
+using API.Data;
 
 namespace API
 {
