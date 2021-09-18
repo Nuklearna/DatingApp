7ce38ed327b181ea6fd9606180f9d6cb2e659100
nuklearna <n.swenda@gmail.com>
2021-09-18 15:44:03 +0200

    Token services and interfaces

--- API/Startup.cs
+++ API/Startup.cs
@@ -14,6 +14,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Microsoft.Data.Sqlite;
 using API.Data;
+using API.Interfaces;
+using API.Services;
 
 namespace API
 {
@@ -28,6 +30,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ITokenService, TokenService>();
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
