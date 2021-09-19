6976e7e7389258dd4c68c7c1cf4635badba5e912
nuklearna <n.swenda@gmail.com>
2021-09-19 16:18:00 +0200

    Add navigation and form for login (draft)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,20 +2,24 @@ import { importExpr } from '@angular/compiler/src/output/output_ast';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
