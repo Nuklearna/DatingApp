f4706b18dc9a19b66f56bdffcb9297cd0ed0f546
nuklearna <n.swenda@gmail.com>
2021-12-11 14:40:59 +0100

    Adding routes

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -18,14 +19,14 @@ export class NavComponent implements OnInit {
   model: any = {};
 
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   login() {
     this.accountService.login(this.model).subscribe(res => {
-      console.log(res)
+      this.router.navigateByUrl('/members');
     }, err => {
       console.log(err)
     })
@@ -33,6 +34,7 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
